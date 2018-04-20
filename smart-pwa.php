35b3fc9e82e2b03c29aa44d4cf63250d69c4b488
Hiroshi Urabe <mail@torounit.com>
2018-04-21 04:38:33 +0900

    Initial commit

---
+++ smart-pwa.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Smart Pwa
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     smart-pwa
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Smart_Pwa
+ */
+
+// Your code starts here.

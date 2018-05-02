98b3a5ac61e6d075772f1db5200cdc6c9a1d3d8b
Hiroshi Urabe <mail@torounit.com>
2018-05-02 16:31:45 +0900

    phpcs fix

--- smart-pwa.php
+++ smart-pwa.php
@@ -1,5 +1,14 @@
 <?php
 /**
+ * Smart PWA
+ *
+ * PWA for WordPress
+ *
+ * @package         Smart_PWA
+ * @version         0.1.0
+ */
+
+/**
  * Plugin Name:     Smart PWA
  * Plugin URI:      https://github.com/torounit/smart-pwa
  * Description:     PWA for WordPress
@@ -8,15 +17,15 @@
  * Text Domain:     smart-pwa
  * Version:         0.1.0
  * Requires PHP:    7.0
- *
- * @package         Smart_PWA
  */
 
 require dirname( __FILE__ ) . '/includes/autoloader.php';
 
 define( 'SMART_PWA_FILE', __FILE__ );
 
-
+/**
+ * Run Plugin.
+ */
 function smart_pwa_init() {
 	new Smart_PWA\App();
 }

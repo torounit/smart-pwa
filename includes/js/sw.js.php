b9a52b2b1c9bc7872ae6b3d97d3df2f279bd7bfa
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2018-05-22 14:48:47 +0900

    fixformat

--- includes/js/sw.js.php
+++ includes/js/sw.js.php
@@ -6,104 +6,106 @@
  */
 
 ?>
-'use strict';
-const APP_SHELL_CACHE_NAME = 'smart-pwa-<?php echo get_transient( 'smart_pwa_hash' ); ?>';
-const RUNTIME_CACHE_NAME = 'smart-pwa-runtime-cache';
-const NOT_AVAILABLE_KEY = '<?php echo '/' . user_trailingslashit( get_page_uri( get_option( 'smart_pwa_not_available_page', false ) ) ); ?>';
+'use strict'
+const APP_SHELL_CACHE_NAME = 'smart-pwa-<?php echo get_transient( 'smart_pwa_hash' ); ?>'
+const RUNTIME_CACHE_NAME = 'smart-pwa-runtime-cache'
+const NOT_AVAILABLE_KEY = '<?php echo '/' . user_trailingslashit( get_page_uri( get_option( 'smart_pwa_not_available_page', false ) ) ); ?>'
+const MANIFEST_URL = '<?php echo '/' . user_trailingslashit( Smart_PWA\MANIFEST_ENDPOINT );?>'
 
-const PRE_CACHE_ASSETS = JSON.parse( '<?php echo json_encode( get_option( 'smart_pwa_assets_paths', [] ) ); ?>' );
+const PRE_CACHE_ASSETS = JSON.parse( '<?php echo json_encode( get_option( 'smart_pwa_assets_paths', [] ) ); ?>' )
 
 const urlsToPreCache = [
 	'/',
+	MANIFEST_URL,
 	NOT_AVAILABLE_KEY,
-].concat( PRE_CACHE_ASSETS );
+].concat( PRE_CACHE_ASSETS )
 
 const updateMessage = ( message ) => {
 	self.clients.matchAll().then( clients =>
-		clients.forEach( client => client.postMessage( message ) ) );
+		clients.forEach( client => client.postMessage( message ) ) )
 }
 
 
 self.addEventListener( 'install', ( event ) => {
-	console.log( '[ServiceWorker] Install' );
+	console.log( '[ServiceWorker] Install' )
 	event.waitUntil(
 		caches.open( APP_SHELL_CACHE_NAME )
 			.then( ( cache ) => {
-				console.log( '[ServiceWorker] Caching app' );
-				return cache.addAll( urlsToPreCache );
+				console.log( '[ServiceWorker] Caching app' )
+				return cache.addAll( urlsToPreCache )
 			} )
-	);
-} );
+	)
+} )
 
 self.addEventListener( 'activate', ( event ) => {
-	console.log( '[ServiceWorker] Activate' );
-	const cacheWhitelist = [ APP_SHELL_CACHE_NAME ];
+	console.log( '[ServiceWorker] Activate' )
+	const cacheWhitelist = [APP_SHELL_CACHE_NAME]
 	event.waitUntil(
 		caches.keys().then( ( cacheNames ) => {
 			return Promise.all(
 				cacheNames.map( ( cacheName ) => {
-					if (cacheWhitelist.indexOf( cacheName ) === - 1) {
-						console.log( '[ServiceWorker] Removing old cache', cacheName );
-						return caches.delete( cacheName );
+					if ( cacheWhitelist.indexOf( cacheName ) === -1 ) {
+						console.log( '[ServiceWorker] Removing old cache', cacheName )
+						return caches.delete( cacheName )
 					}
 				} )
-			);
+			)
 		} )
-	);
-} );
+	)
+} )
 
 
 self.addEventListener( 'fetch', ( event ) => {
 	if (
-		event.request.url.indexOf( 'wp-admin' ) === - 1 &&
-		event.request.url.indexOf( 'wp-login' ) === - 1 &&
-		event.request.url.indexOf( 'customize_changeset_uuid' ) === - 1 &&
+		event.request.url.indexOf( 'wp-admin' ) === -1 &&
+		event.request.url.indexOf( 'wp-login' ) === -1 &&
+		event.request.url.indexOf( 'customize_changeset_uuid' ) === -1 &&
 		event.request.method === 'GET'
 	) {
 		event.respondWith(
 			caches.match( event.request ).then( ( responseFromCache ) => {
 
 				//Return static content in cache.
-				if (responseFromCache) {
-					if ([ 'style', 'script', 'image' ].indexOf( event.request.destination ) > - 1) {
-						return responseFromCache;
+				if ( responseFromCache ) {
+					if ( ['style', 'script', 'image'].indexOf( event.request.destination ) > -1 ) {
+						return responseFromCache
 					}
 				}
 
 				//Fetch and save content to cache.
 				return fetch( event.request ).then( ( response ) => {
 					//Not 200.
-					if (! response || response.status !== 200 || response.type !== 'basic') {
-						return response;
+					if ( !response || response.status !== 200 || response.type !== 'basic' ) {
+						return response
 					}
 
-					let responseToCache = response.clone();
+					let responseToCache = response.clone()
 					caches.open( RUNTIME_CACHE_NAME ).then( cache => {
-						cache.put( event.request, responseToCache );
-						console.log( '[ServiceWorker] Fetched&Cached Data', event.request.url );
+						cache.put( event.request, responseToCache )
+						console.log( '[ServiceWorker] Fetched&Cached Data', event.request.url )
 						updateMessage( {
 							key: 'updateContent',
 							value: event.request.url
-						} );
+						} )
 
-					} );
-					return response;
+					} )
+					return response
 
 				} ).catch( ( reason ) => {
 					//when fetching failed, return content in cache.
-					if (responseFromCache) {
-						console.log( '[ServiceWorker] Cache Matched!', event.request.url, responseFromCache );
-						return responseFromCache;
+					if ( responseFromCache ) {
+						console.log( '[ServiceWorker] Cache Matched!', event.request.url, responseFromCache )
+						return responseFromCache
 					}
 					//if not found in cache, return fallback page.
-					if (event.request.mode === 'navigate') {
+					if ( event.request.mode === 'navigate' ) {
 						return caches.match( NOT_AVAILABLE_KEY ).then( ( response ) => {
-							return response;
-						} );
+							return response
+						} )
 					}
-				} );
+				} )
 			} )
-		);
+		)
 	}
-} );
+} )
 

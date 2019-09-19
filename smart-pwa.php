0aaa85509e7d633c20b9d57ba068779e626f6306
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-09-19 12:19:56 +0900

    update internal ver

--- smart-pwa.php
+++ smart-pwa.php
@@ -15,7 +15,7 @@
  * Author:          Toro_Unit
  * Author URI:      https://torounit.com
  * Text Domain:     smart-pwa
- * Version:         0.1.6
+ * Version:         0.1.7
  * Requires PHP:    7.0
  */
 
